ae84878ba257ce47797b41c691d23c581db44638
ISupertwister <Ogz.frkn.clk.37@hotmail.com>
2023-11-13 21:00:24 +0300

    First Commit

---
+++ Assets/Scripts/Game/LevelEvents.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LevelEvents : MonoBehaviour
+{
+    public GameObject gamePausedPanel;
+    public Button pauseButton; 
+
+    private void Update()
+    {
+        if (!PlayerManager.isGameStarted)
+            return;
+
+        if (PlayerManager.gameOver)
+            pauseButton.interactable = false;
+
+        if (PlayerManager.gameOver)
+            return;
+
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PlayerManager.isGamePaused)
+            {
+                ResumeGame();
+                gamePausedPanel.SetActive(false);
+            }
+            else
+            {
+                PauseGame();
+                gamePausedPanel.SetActive(true);
+            }
+
+        }
+    }
+    public void ReplayGame1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void ReplayGame2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void PauseGame()
+    {
+        if (!PlayerManager.isGamePaused && !PlayerManager.gameOver)
+        {
+            Time.timeScale = 0;
+            PlayerManager.isGamePaused = true;
+        }
+    }
+    public void ResumeGame()
+    {
+        if (PlayerManager.isGamePaused)
+        {
+            Time.timeScale = 1;
+            PlayerManager.isGamePaused = false;
+        }
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ Assets/Scripts/Game/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    public TextMeshProUGUI highScoreText;
+    public TextMeshProUGUI gemsText;
+
+    public Animator messageAnim;
+
+    public GameObject levelepanel;
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+    private void Update()
+    {
+        highScoreText.text = "High Score\n" + PlayerPrefs.GetInt("HighScore", 0);
+        gemsText.text = PlayerPrefs.GetInt("TotalGems", 0).ToString();
+    }
+    public void PlayGame()
+    {
+        levelepanel.SetActive(true);
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void exit()
+    {
+        levelepanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
